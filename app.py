fc6f48436ee8887dcf52d9193da0e01baa18fc7a
Sarthak Goyal <goyalsarthak847@gmail.com>
2026-03-12 15:20:29 +0530

    Update app.py to use correct random_forest_model.pkl

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load the trained model
-model = joblib.load("rf_model.pkl")
+model = joblib.load("random_forest_model.pkl")
 
 @app.route('/')
 def home():
